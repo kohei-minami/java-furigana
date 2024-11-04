b8c68ed1b6ba7fb9979a1275ed3df12bdaaa68e1
minamidazooo <110703210+kohei-minami@users.noreply.github.com>
2024-11-04 10:23:30 +0900

    Create chapter1.js

---
+++ study/chapter1.js
@@ -0,0 +1,29 @@
+console.log('ハロー！');
+
+console.log(10+5);
+console.log(10-5);
+
+console.log(2+10+5);
+console.log((2+10)*5);
+
+let moji = 'ハロー';
+console.log(moji);
+
+let price = 100;
+let tax = price * 0.1;
+console.log(tax);
+
+let price = 150;
+let tax = price * 0.1;
+console.log(tax);
+
+let moji = prompt('入力せよ');
+console.log(moji);
+
+let moji = prompt('入力せよ');
+console.log('入力したのは' + moji);
+
+let price = prompt('価格を入力せよ');
+console.log(parseInt(price)+80);
+
+console.log(`ハロー!`,10,3.5)
